b6d190c167cd54aad1c0619dc4a01996c50a38aa
MaryselR <maryselr@gmail.com>
2020-08-22 04:18:01 -0300

    First commit

---
+++ SeleniumAutomation/src/test/java/com/javabasic/Datatypes.java
@@ -0,0 +1,31 @@
+package com.javabasic;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		byte x;
+//		short y;
+//		int z;
+//		long a;
+//		
+//		x=127;
+//		y=32767;
+//		z=2147480647;
+//		a=99999999999999L;
+//		
+//		System.out.print(x);
+
+		float base;
+		double altura;
+		
+		base = 30.5F;
+		altura = 40.76;
+				
+		System.out.println(altura);
+
+		
+	}
+
+}
